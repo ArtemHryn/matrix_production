ef342d0508b7ded1eb2b98e07ac78e7355a66e3e
Artem <grynbest@gmail.com>
2023-07-27 14:22:35 +0300

    removed authors

--- src/components/CalculatorPageComponents/ChangeYourFate/ChangeYourFate.jsx
+++ src/components/CalculatorPageComponents/ChangeYourFate/ChangeYourFate.jsx
@@ -1,54 +1,54 @@
-import { useTranslation } from 'react-i18next';
-import { Box } from 'components/Box';
+import { useTranslation } from "react-i18next";
+import { Box } from "components/Box";
 import {
   AssistanseText,
-  CalcAuthor,
+  // CalcAuthor,
   ConsultationBtn,
   Image,
   Karmolog,
   Line,
   Logo,
   Title,
-} from './ChangeYourFate.styled';
-import authorMob from 'images/Calculator/ChangeYourFate/AuthorPhoto.webp';
-import authorTab from 'images/Calculator/ChangeYourFate/authorTab.webp';
-import authorDesk from 'images/Calculator/ChangeYourFate/authorDesk.webp';
+} from "./ChangeYourFate.styled";
+import authorMob from "images/Calculator/ChangeYourFate/AuthorPhoto.webp";
+import authorTab from "images/Calculator/ChangeYourFate/authorTab.webp";
+import authorDesk from "images/Calculator/ChangeYourFate/authorDesk.webp";
 
 export const ChangeYourFate = () => {
-  const {t} =  useTranslation('calc')
+  const { t } = useTranslation("calc");
   return (
-    <Box id="ChangeYourFate" as="section" py={['40px']}>
+    <Box id="ChangeYourFate" as="section" py={["40px"]}>
       <Box
-        maxWidth={['400px', '768px', '1440px']}
-        px={['37px', '32px', '192px']}
+        maxWidth={["400px", "768px", "1440px"]}
+        px={["37px", "32px", "192px"]}
         m="0 auto"
       >
         <Box
-          backgroundColor={[null, 'rgba(255, 255, 255, 0.5);']}
-          borderRadius={[null, '23px']}
-          display={[null, 'flex']}
+          backgroundColor={[null, "rgba(255, 255, 255, 0.5);"]}
+          borderRadius={[null, "23px"]}
+          display={[null, "flex"]}
           overflow="hidden"
-          mb={['25px', '29px']}
-          justifyContent={[null, 'space-between']}
+          mb={["25px", "29px"]}
+          justifyContent={[null, "space-between"]}
         >
           <Box
-            mb={['7px', '0px']}
-            py={[null, '42px', '36px']}
-            pl={[null, '40px', '66px']}
-            pr={[null, '11px']}
-            maxWidth={[null, '60%', '65%']}
+            mb={["7px", "0px"]}
+            py={[null, "42px", "36px"]}
+            pl={[null, "40px", "66px"]}
+            pr={[null, "11px"]}
+            maxWidth={[null, "60%", "65%"]}
           >
-            {' '}
-            <Box ml={[null, 'auto']}>
+            {" "}
+            <Box ml={[null, "auto"]}>
               <Logo>Dari.Karma</Logo>
               <Line>
                 <Karmolog>Кармолог</Karmolog>
               </Line>
             </Box>
-            <Title>{t('changeYourFateTitle')}</Title>
-            <AssistanseText>{t('changeYourFateAssistanseText')}</AssistanseText>
-            <ConsultationBtn to="/" state={{ from: '4methods' }}>
-              {t('changeYourFateConsultationBtn')}
+            <Title>{t("changeYourFateTitle")}</Title>
+            <AssistanseText>{t("changeYourFateAssistanseText")}</AssistanseText>
+            <ConsultationBtn to="/" state={{ from: "4methods" }}>
+              {t("changeYourFateConsultationBtn")}
             </ConsultationBtn>
           </Box>
           <picture>
@@ -57,7 +57,7 @@ export const ChangeYourFate = () => {
             <Image src={authorMob} alt="author" />
           </picture>
         </Box>
-        <CalcAuthor>
+        {/* <CalcAuthor>
           {t('changeYourFateCalcAuthor1')}{' '}
           <CalcAuthor as="span" fontWeight="700" fontStyle="italic">
             {t('changeYourFateCalcAuthor2')}
@@ -66,7 +66,7 @@ export const ChangeYourFate = () => {
           <CalcAuthor as="span" fontWeight="700" fontStyle="italic">
             {t('changeYourFateCalcAuthor4')}
           </CalcAuthor>
-        </CalcAuthor>
+        </CalcAuthor> */}
       </Box>
     </Box>
   );
